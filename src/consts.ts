cfd074bc2dd9b370d9276b803bba99b2b20d4316
OwenRThomson <92429185+OwenRThomson@users.noreply.github.com>
2025-06-21 20:37:37 +0100

    Update to use yarn

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "SomeName";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "OwenFromWork";
+export const SITE_DESCRIPTION =
+  "A blog about software engineering and frontend development for data analytics products.";
 export const COLOUR_THEME = "concord";
 export const THEME = "dark"; // or "dark"
-export const YOUR_NAME = "Someone"; // Your name or the name of the site owner
+export const YOUR_NAME = "owen"; // Your name or the name of the site owner

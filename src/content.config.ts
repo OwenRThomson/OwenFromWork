282f41a50477c2c029ad086f8297239ccfb73b14
OwenRThomson <92429185+OwenRThomson@users.noreply.github.com>
2025-06-21 22:41:18 +0100

    refactor: :recycle: Set Projects as Github Repos

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,20 +17,4 @@ const blogPosts = defineCollection({
     }),
 });
 
-const projects = defineCollection({
-  // Load Markdown and MDX files in the `src/content/project/` directory.
-  loader: glob({ base: "./src/content/project", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string().optional(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
-      tags: z.array(z.string()).default([]),
-    }),
-});
-
-export const collections = { blogPosts, projects };
+export const collections = { blogPosts };
